8efe15bde0fb28e0d122d392024ddf16141427d9
Pierre TOP (TOPI) <ptop@octo.com>
2021-05-21 18:20:49 +0200

    [API] Add findTagIdsByOrganizationId to organizationTagRepository

--- api/lib/infrastructure/repositories/organization-tag-repository.js
+++ api/lib/infrastructure/repositories/organization-tag-repository.js
@@ -1,6 +1,8 @@
 const BookshelfOrganizationTag = require('../orm-models/OrganizationTag');
 const Bookshelf = require('../bookshelf');
-const bookshelfUtils = require('../utils/knex-utils');
+const knexUtils = require('../utils/knex-utils');
+const { knex } = require('../../../db/knex-database-connection');
+
 const bookshelfToDomainConverter = require('../utils/bookshelf-to-domain-converter');
 const { AlreadyExistingEntityError } = require('../../domain/errors');
 const { omit } = require('lodash');
@@ -14,7 +16,7 @@ module.exports = {
       const bookshelfOrganizationTag = await new BookshelfOrganizationTag(organizationTagToCreate).save();
       return bookshelfToDomainConverter.buildDomainObject(BookshelfOrganizationTag, bookshelfOrganizationTag);
     } catch (err) {
-      if (bookshelfUtils.isUniqConstraintViolated(err)) {
+      if (knexUtils.isUniqConstraintViolated(err)) {
         throw new AlreadyExistingEntityError(`The tag ${organizationTag.tagId} already exists for the organization ${organizationTag.organizationId}.`);
       }
       throw err;
@@ -31,5 +33,9 @@ module.exports = {
       .fetch({ require: false });
 
     return !!organizationTag;
+  },
+
+  async findTagIdsByOrganizationId({ organizationId }) {
+    return knex.from('organization-tags').where({organizationId}).pluck('tagId');
   },
 };
