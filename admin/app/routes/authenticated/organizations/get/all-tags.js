8776bb1806d7b777ed4ca5979ebe6aba98a8ceb8
Anne-G <ang.schall@gmail.com>
2021-05-26 11:06:07 +0200

    [ADMIN] Return orga model to orga-all-tags component

--- admin/app/routes/authenticated/organizations/get/all-tags.js
+++ admin/app/routes/authenticated/organizations/get/all-tags.js
@@ -3,9 +3,9 @@ import Route from '@ember/routing/route';
 export default class AllTags extends Route {
 
   async model() {
-    const organizationTags = this.modelFor('authenticated.organizations.get').tags;
+    const organization = this.modelFor('authenticated.organizations.get');
     const allTags = await this.store.findAll('tag');
 
-    return { organizationTags, allTags };
+    return { organization, allTags };
   }
 }
